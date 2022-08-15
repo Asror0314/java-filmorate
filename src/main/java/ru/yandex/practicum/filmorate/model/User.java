5bb7a19e6553cf5aa21ef1dd565ba07195795ea8
asror <hudoykulov.asror95@gmail.com>
2022-08-15 16:31:28 +0500

    refactored class User

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -5,12 +5,18 @@ import lombok.*;
 import java.time.LocalDate;
 
 @Data
+@EqualsAndHashCode
 public class User {
 
+    @EqualsAndHashCode.Include
     private int id;
+    @EqualsAndHashCode.Exclude
     private String name;
+    @EqualsAndHashCode.Exclude
     private String email;
+    @EqualsAndHashCode.Exclude
     private String login;
+    @EqualsAndHashCode.Exclude
     private LocalDate birthday;
 
 }
